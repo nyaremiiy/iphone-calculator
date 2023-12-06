154e6c57d69bb082e8f04b302ba8de56d4c97956
Nazar Yaremiiy <nazar.yaremiiy@gmail.com>
2023-12-06 13:46:36 +0200

    Added head. Added icons and time

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log('App start!!!');
+const time = {
+  getHours: function () {
+    return new Date().getHours();
+  },
+  getMinutes: function () {
+    return new Date().getMinutes();
+  },
+  getStringTime: function () {
+    let hours = this.getHours(),
+      minutes = this.getMinutes();
+    if (this.getHours() >= 0 && this.getHours() <= 9) {
+      hours = `0${this.getHours()}`;
+    }
+    if (this.getMinutes() >= 0 && this.getMinutes() <= 9) {
+      hours = `0${this.getMinutes()}`;
+    }
+    return `${hours}:${minutes}`;
+  },
+};
+
+setInterval(() => {
+  document.querySelector("#js-time").textContent = time.getStringTime();
+}, 500);
